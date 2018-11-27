f8ea3af83cd738da40834f9da878398f19a517ac
Rajiv-Patel <43829715+Rajiv-Patel@users.noreply.github.com>
2018-11-26 21:45:59 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,15 +6,15 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('/dist/FirstApp'));
+app.use(express.static('./dist/FirstApp'));
 // app.use(express.static(__dirname + '/dist/<RajApp-Comp>'));
 
 // app.get('/*', function(req,res) {    
 // res.sendFile(path.join(__dirname+'/dist/<RajApp-Comp>/index.html'));
 // });
 
-app.get('/*all', function(req, res) {
-  res.sendFile(path.join('/dist/FirstApp/index.html'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join('./dist/FirstApp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
