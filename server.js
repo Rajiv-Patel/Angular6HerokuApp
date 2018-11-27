e5f483d0015c395268e48d24ee4a1c48ed4c53d8
Rajiv-Patel <43829715+Rajiv-Patel@users.noreply.github.com>
2018-11-27 12:03:14 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,47 @@
 
-//Install express server
-const express = require('express');
-const path = require('path');
-//const routes = require("./routes");
-const app = express();
+// //Install express server
+// const express = require('express');
+// const path = require('path');
+// //const routes = require("./routes");
+// const app = express();
 
 
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/firstapp'));
-// app.use(express.static(__dirname + '/dist/<RajApp-Comp>'));
-//app.use("/",routes);
+// // Serve only the static files form the dist directory
+// app.use(express.static('./dist/firstapp'));
+// // app.use(express.static(__dirname + '/dist/<RajApp-Comp>'));
+// //app.use("/",routes);
 
-// app.get('/*', function(req,res) {    
-// res.sendFile(path.join(__dirname+'/dist/<RajApp-Comp>/index.html'));
+// // app.get('/*', function(req,res) {    
+// // res.sendFile(path.join(__dirname+'/dist/<RajApp-Comp>/index.html'));
+// // });
+
+// app.get('/*', function(req, res) {
+//   res.sendFile(path.join('./dist/firstapp/index.html'));
 // });
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join('./dist/firstapp/index.html'));
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 8080;
+const server = require('http').Server(app);
+
+app.use(express.static(__dirname, 'dist', {index: false}));
+
+
+server.listen(port, function() {
+    console.log("App running on port " + port);
+})
+
+// PathLocationStrategy
+
+app.get('', function(req, res) {
+    res.sendFile(path.join(__dirname, 'src', 'index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'src', 'index.html'));
+});
