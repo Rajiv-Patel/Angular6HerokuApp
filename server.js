7d57461ca92299c8c3cf9cb11e30dab3d3216ea6
Rajiv-Patel <43829715+Rajiv-Patel@users.noreply.github.com>
2018-11-26 21:10:06 -0500

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static(__dirname + '/dist/RajApp-Comp'));
 // });
 
 app.get('/*all', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/RajApp-Comp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
